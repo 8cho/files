77931e0fa0fcbc2c63308be82b4290115f48d660
gamba <gambatesem@gmail.com>
2021-01-14 13:02:57 -0800

    quotes

--- random-greeting.js
+++ random-greeting.js
@@ -2,9 +2,9 @@ var greeting = [
   "Oh, hi!",
   "Buenas",
   "Hi there",
-  "やぁ"
+  "やぁ〜"
 ];
 
 var randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
 
-document.getElementById("quote").innerHTML = randomGreeting;
+document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
