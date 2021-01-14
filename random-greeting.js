05bf9ecf310e02ed90f2e666dc55ff4c012980f5
no <gambatesem@gmail.com>
2021-01-14 13:17:55 -0800

    function

--- random-greeting.js
+++ random-greeting.js
@@ -1,10 +1,13 @@
-var greeting = [
-  "Oh, hi!",
-  "Buenas",
-  "Hi there",
-  "やぁ〜"
-];
+function shuffleGreeting() {
+  var greeting = [
+    "Oh, hi!",
+    "Buenas",
+    "Hi there",
+    "やぁ〜"
+  ];
 
-var randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
+  var randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
 
-document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
+  document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
+}
+shuffleGreeting();
