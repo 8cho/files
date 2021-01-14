1c808696fc84ff6c52f6436bd674b49601f6b007
no <gambatesem@gmail.com>
2021-01-14 13:36:52 -0800

    again

--- random-greeting.js
+++ random-greeting.js
@@ -1,4 +1,4 @@
-function shuffleGreeting() {
+document.addEventListener("DOMContentLoaded", shuffleGreeting() {
   var greeting = [
     "Oh, hi!",
     "Buenas",
@@ -9,5 +9,4 @@ function shuffleGreeting() {
   var randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
 
   document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
-}
-document.addEventListener("DOMContentLoaded", shuffleGreeting());
+});
