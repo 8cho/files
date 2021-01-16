8e735c6d2cb90ad8374e697b9b0dab8b204a834a
gamba <gambatesem@gmail.com>
2021-01-15 20:36:25 -0800

    STAHP

--- random-greeting.js
+++ random-greeting.js
@@ -1,46 +1,53 @@
-function shuffleGreeting() {
-  var greeting = [
+let clickCount = 0;
+const greeting = [
 //GREETINGS
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
+  "Oh, hi!",
+  "Buenas",
+  "Hi there",
+  "やぁ〜",
+  "'sup",
+  "Ciao!",
 //GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
+  "Oh, hi!",
+  "Buenas",
+  "Hi there",
+  "やぁ〜",
+  "'sup",
+  "Ciao!",
 //GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
+  "Oh, hi!",
+  "Buenas",
+  "Hi there",
+  "やぁ〜",
+  "'sup",
+  "Ciao!",
 //GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
+  "Oh, hi!",
+  "Buenas",
+  "Hi there",
+  "やぁ〜",
+  "'sup",
+  "Ciao!",
 //GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
+  "Oh, hi!",
+  "Buenas",
+  "Hi there",
+  "やぁ〜",
+  "'sup",
+  "Ciao!",
 //UNLIKELY QUOTES
-    "42"
-  ];
+  "42"
+];
 
-  var randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
-
-  document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
+function shuffleGreeting() {
+  clickCount++;
+  if (clickCount<10) {
+    const randomGreeting = greeting[Math.floor(Math.random()*greeting.length)];
+    document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
+  } else {
+    document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = "STAHP!";
+  }
 }
+
 shuffleGreeting();
+document.getElementsByClassName('notion-breadcrumb__item')[0].addEventListener('click',shuffleGreeting);
