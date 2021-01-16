7ed75c499d9a9f5b520538fbd91987861c3a9cf8
gamba <gambatesem@gmail.com>
2021-01-15 19:45:36 -0800

    onclick

--- random-greeting.js
+++ random-greeting.js
@@ -35,41 +35,6 @@ function shuffleGreeting() {
     "やぁ〜",
     "'sup",
     "Ciao!",
-//GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
-//GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
-//GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
-//GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
-//GREETINGS RATE++
-    "Oh, hi!",
-    "Buenas",
-    "Hi there",
-    "やぁ〜",
-    "'sup",
-    "Ciao!",
 //UNLIKELY QUOTES
     "42"
   ];
@@ -79,3 +44,4 @@ function shuffleGreeting() {
   document.getElementById("block-cbc6e7f3e0cc4a098a0ab1163de9b210").getElementsByClassName("notion-semantic-string")[0].innerHTML = randomGreeting;
 }
 shuffleGreeting();
+window.onclick = shuffleGreeting();
